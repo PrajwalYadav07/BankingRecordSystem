999984c8e426b9e9b52207084d4c84fa7e994526
Prajwal Yadav <134254781+PrajwalYadav07@users.noreply.github.com>
2025-08-29 21:29:06 +0530

    Update banking record.cpp
    minor change

--- banking record.cpp
+++ banking record.cpp
@@ -2,14 +2,7 @@
 #include<iostream>
 #include<fstream>
 #include<cstdlib>
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::fstream;
-using std::ofstream;
-using std::ifstream;
-using std::ios;
+using namespace std;
 
 class account_query {
     private:
@@ -179,4 +172,5 @@ int main () {
     }    
     system("pause");
     return 0;    
+
 }
